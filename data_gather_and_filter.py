ce958ba9dc0a020a4ae6791b61c68b0b0fc31630
Yihan Li <yihanli@bu.edu>
2023-11-29 20:07:22 -0500

    new base data from sp500

--- data_gather_and_filter.py
+++ data_gather_and_filter.py
@@ -10,70 +10,71 @@ import yfinance as yf
 import pandas as pd
 import os
 from tqdm import tqdm
+import yahoo_fin.stock_info as si
 
 
-def get_tickers(file_path, list_time_max):
-    data = pd.read_csv(file_path)
-    filtered_data = data[(data['IPO Year'] <= list_time_max) | (data['IPO Year'].isna())]
-    print(filtered_data)
-    symbol_list = filtered_data['Symbol'].tolist()
-    
-    return symbol_list
-    
 
-file_name_list = ['NASDAQ.csv', 'NYSE.csv', 'AMEX.csv']
-symbols = []
-for file in file_name_list:
-    symbol_list = get_tickers(file, 2022)
-    print(len(symbol_list))
-    symbols += symbol_list
-print(len(symbols))
-
+symbols = si.tickers_sp500()
 
 def getHistory(symbols, period, start_date, end_date, save_path=None):
     Close = pd.DataFrame()
     Open = pd.DataFrame()
+    Volume =pd.DataFrame()
 
     for ticker in tqdm(symbols):
         ticker_object = yf.Ticker(ticker)
         ticker_historical = ticker_object.history(period=period, start=start_date, end=end_date)
         Close = pd.concat([Close, ticker_historical['Close']], axis=1)
         Open = pd.concat([Open, ticker_historical['Open']], axis=1)
+        Volume = pd.concat([Volume, ticker_historical['Volume']], axis=1)
     
     Close.columns = symbols
     Open.columns = symbols
+    Volume.columns = symbols
 
     if save_path:
-        Close.to_csv(os.path.join(save_path, 'Close.csv'))
-        Open.to_csv(os.path.join(save_path, 'Open.csv'))
+        Close.to_csv(os.path.join(save_path, 'Close_sp500.csv'))
+        Open.to_csv(os.path.join(save_path, 'Open_sp500.csv'))
+        Volume.to_csv(os.path.join(save_path, 'Volume_sp500.csv'))
         
 
     return Close, Open
 
 
 
-getHistory(symbols, period = '1d', start_date = '2011-01-01', end_date = '2023-11-22', save_path = 'original data')
-Close = pd.read_csv('original data/Close.csv', index_col=0)
+getHistory(symbols, period = '1d', start_date = '2012-12-01', end_date = '2023-11-28', save_path = 'original data')
+Close = pd.read_csv('original data/Close_sp500.csv', index_col=0)
 Close.index = pd.to_datetime(Close.index)
 close_non_null_counts = Close.count()
 close_threshold = 0.75 * len(Close)
 close_data = Close.loc[:, close_non_null_counts >= close_threshold]
-close_data.index = pd.to_datetime(close_data.index, utc=True).tz_localize(None).date
-month_counts = close_data.index.to_period("M").value_counts()
+close_data.index = pd.to_datetime(close_data.index, utc=True).tz_localize(None)
+close_data.index = close_data.index.to_period("M")
+month_counts = close_data.index.value_counts()
 under_15_months = month_counts[month_counts < 15].index
-close_data = close_data[~close_data.index.to_period("M").isin(under_15_months)]
-close_data.to_csv('filtered_data/close_data.csv')
+close_data = close_data[~close_data.index.isin(under_15_months)]
+close_data.to_csv('filtered_data/close_data_sp500.csv')
 
 
-Open = pd.read_csv('original data/Open.csv', index_col=0)
+Open = pd.read_csv('original data/Open_sp500.csv', index_col=0)
 Open.index = pd.to_datetime(Open.index)
 open_non_null_counts = Open.count()
 open_threshold = 0.75 * len(Open)
 open_data = Open.loc[:, open_non_null_counts >= open_threshold]
-open_data.index = pd.to_datetime(open_data.index, utc=True).tz_localize(None).date
-open_data = open_data[~open_data.index.to_period("M").isin(under_15_months)]
-open_data.to_csv('filtered_data/open_data.csv')
+open_data.index = pd.to_datetime(open_data.index, utc=True).tz_localize(None)
+open_data.index = open_data.index.to_period("M")
+open_data = open_data[~open_data.index.isin(under_15_months)]
+open_data.to_csv('filtered_data/open_data_sp500.csv')
 
+Volume = pd.read_csv('original data/Volume_sp500.csv', index_col=0)
+Volume.index = pd.to_datetime(Volume.index)
+Volume_non_null_counts = Volume.count()
+Volume_threshold = 0.75 * len(Volume)
+Volume_data = Volume.loc[:, Volume_non_null_counts >= Volume_threshold]
+Volume_data.index = pd.to_datetime(Volume_data.index, utc=True).tz_localize(None)
+Volume_data.index = Volume_data.index.to_period("M")
+Volume_data = Volume_data[~Volume_data.index.isin(under_15_months)]
+Volume_data.to_csv('filtered_data/volume_data_sp500.csv')
 
 
 
---
+++ new_variable_calculation.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 29 14:37:33 2023
+
+@author: liyihan
+"""
+
+import pandas as pd
+close_data = pd.read_csv('filtered_data/close_data_sp500.csv', index_col=0)
+close_data.index = pd.to_datetime(close_data.index)
+close_data.index = close_data.index.to_period("M")
+open_data = pd.read_csv('filtered_data/open_data_sp500.csv', index_col=0)
+open_data.index = pd.to_datetime(open_data.index)
+
+open_data.index = open_data.index.to_period("M")
+
+
+daytime_return = (close_data / open_data) - 1
+daytime_return = daytime_return[daytime_return.index >= '2013-01']
+
+
+daily_return = (close_data / close_data.shift(1) - 1)
+daily_return = daily_return[daily_return.index >= '2013-01']
+
+
+overnight_return = (1 + daily_return) / (1 + daytime_return) - 1
+
+
+monthly_close = close_data.groupby(close_data.index).last()
+monthly_open = open_data.groupby(open_data.index).first()
+monthly_return = (monthly_close / monthly_open) - 1
+
+
+pos_reversal = ((overnight_return > 0) & (daytime_return < 0)).astype(int)
+PR_ratio = pos_reversal.groupby(pos_reversal.index).mean()
+
+neg_reversal = ((overnight_return < 0) & (daytime_return > 0)).astype(int)
+NR_ratio = neg_reversal.groupby(neg_reversal.index).mean()
+
+
+AB_PR = PR_ratio.rolling(window=12).mean()
+AB_PR = AB_PR[AB_PR.index >= '2014-01']
+PR_ratio = PR_ratio[PR_ratio.index >= '2014-01']
+AB_PR = AB_PR * PR_ratio
+
+
+AB_NR = NR_ratio.rolling(window=12).mean()
+AB_NR = AB_NR[AB_NR.index >= '2014-01']
+NR_ratio = NR_ratio[NR_ratio.index >= '2014-01']
+AB_NR = AB_NR * NR_ratio
+
+
+
+daytime_return.to_csv('variable_results/sp500/daytime_returns.csv')
+daily_return.to_csv('variable_results/sp500/daily_returns.csv')
+monthly_return.to_csv('variable_results/sp500/monthly_returns.csv')
+overnight_return.to_csv('variable_results/sp500/overnight_returns.csv')
+PR_ratio.to_csv('variable_results/sp500/positive_ratio.csv')
+NR_ratio.to_csv('variable_results/sp500/negative_ratio.csv')
+AB_PR.to_csv('variable_results/sp500/bnormal_positive_ratio.csv')
+AB_NR.to_csv('variable_results/sp500/abnormal_negative_ratio.csv')
